e5c02e18c47b0bf36792e39a0741cfc877f173a4
RitaAjdini <ra61370@ubt-uni.net>
2022-12-24 00:08:24 +0100

    Slider qe leviz automatikisht,jo me butona

--- Javascript/homePage.js
+++ Javascript/homePage.js
@@ -19,27 +19,35 @@ document.querySelectorAll(".nav-link").forEach(n=>n.
 //slider 
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
+var vargu=[
+   
+    'Images/MainNavBar/Image1.webp',
+    'Images/MainNavBar/Image3.jpg',
+    'Images/MainNavBar/Image5.webp',
+    'Images/MainNavBar/Image6.webp',
+    'Images/MainNavBar/Image7.webp',
+    'Images/MainNavBar/Image8.webp',
+    'Images/MainNavBar/Image9.jpg',
+    'Images/MainNavBar/Image10.jpg',
+    
+  ];
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  
+  var index=0;
+  var koha=2800;
+  
+  
+  function krijoSlider(){
+  document.getElementById('imazhi').src=vargu[index];
+  index++;
+  if(index==vargu.length){
+  index=0;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active1";
-} 
+  
+  setTimeout("krijoSlider()",koha);
+  }
+
+  krijoSlider();
+  
