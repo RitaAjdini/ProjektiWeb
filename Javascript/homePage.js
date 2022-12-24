4f8c7c2b540f8a9150368a71398ada826a77ab65
vesberisha <vb58972@ubt-uni.net>
2022-12-24 01:04:39 +0100

    shtova thenien,foton me peshkrim,sliderin

--- Javascript/homePage.js
+++ Javascript/homePage.js
@@ -19,27 +19,96 @@ document.querySelectorAll(".nav-link").forEach(n=>n.
 //slider 
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
+var vargu=[
+   
+    'Images/MainNavBar/Image1.webp',
+    'Images/MainNavBar/Image2.jpg',
+    'Images/MainNavBar/Image3.jpg',
+    'Images/MainNavBar/Image4.webp',
+    'Images/MainNavBar/Image5.webp',
+    'Images/MainNavBar/Image6.webp',
+    'Images/MainNavBar/Image7.webp',
+    'Images/MainNavBar/Image8.webp',
+    'Images/MainNavBar/Image9.jpg',
+    'Images/MainNavBar/Image10.jpg',
+    
+  ];
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  
+  var index=0;
+  var koha=2500;
+  
+  
+  function krijoSlider(){
+  document.getElementById('imazhi').src=vargu[index];
+  index++;
+  if(index==vargu.length){
+  index=0;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active1";
-} 
+  
+  setTimeout("krijoSlider()",koha);
+  }
+
+  krijoSlider();
+
+  //teksti me slider
+  const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  
+  let currentSlide = 0;
+  let slideInterval = setInterval(nextSlide, 2500);
+  
+  function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }
+  
+//lojerave slider
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slides2 = document.querySelectorAll('.slide2');
+
+let currentSlide2 = 0;
+
+// Hide all but the first four slides
+for (let i = 4; i < slides2.length; i++) {
+  slides2[i].style.display = 'none';
+}
+
+prevButton.addEventListener('click', () => {
+  // Hide the current set of four slides
+  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
+    slides2[i].style.display = 'none';
+  }
+  // Decrement the current slide index and update the display of the previous set of four slides
+  currentSlide2 = (currentSlide2 - 4 + slides2.length) % slides2.length;
+  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
+    slides2[i].style.display = 'block';
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  // Hide the current set of four slides
+  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
+    slides2[i].style.display = 'none';
+  }
+  // Increment the current slide index
+  currentSlide2 = (currentSlide2 + 4) % slides2.length;
+  // If there are fewer than four slides remaining, show the remaining slides and then show the first four slides again
+  if (currentSlide2 + 4 > slides2.length) {
+    let remainingSlides = currentSlide2 + 4 - slides2.length;
+    for (let i = currentSlide2; i < slides2.length; i++) {
+      slides2[i].style.display = 'block';
+    }
+    for (let i = 0; i < 4; i++) {
+      slides2[i].style.display = 'block';
+    }
+  }
+  // Update the display of the next set of four slides
+  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
+    slides2[i].style.display = 'block';
+  }
+});
