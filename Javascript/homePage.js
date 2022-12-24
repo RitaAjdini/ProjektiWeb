c5c0bb1c992f69f84f913ddcf324ee3694f7c178
vesberisha <vb58972@ubt-uni.net>
2022-12-24 15:35:07 +0100

    shtova slider

--- Javascript/homePage.js
+++ Javascript/homePage.js
@@ -3,112 +3,95 @@ const navMenu = document.querySelector(".nav-menu");
 
 
 
-hamburger.addEventListener("click",()=>{
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 })
 
 
 
-document.querySelectorAll(".nav-link").forEach(n=>n.
-    addEventListener("click",()=>{
+document.querySelectorAll(".nav-link").forEach(n => n.
+  addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  }))
 
 //slider 
 
 
-var vargu=[
-   
-    'Images/MainNavBar/Image1.webp',
-    'Images/MainNavBar/Image2.jpg',
-    'Images/MainNavBar/Image3.jpg',
-    'Images/MainNavBar/Image4.webp',
-    'Images/MainNavBar/Image5.webp',
-    'Images/MainNavBar/Image6.webp',
-    'Images/MainNavBar/Image7.webp',
-    'Images/MainNavBar/Image8.webp',
-    'Images/MainNavBar/Image9.jpg',
-    'Images/MainNavBar/Image10.jpg',
-    
-  ];
+var vargu = [
+
+  'Images/MainNavBar/Image1.webp',
+  'Images/MainNavBar/Image2.jpg',
+  'Images/MainNavBar/Image3.jpg',
+  'Images/MainNavBar/Image4.webp',
+  'Images/MainNavBar/Image5.webp',
+  'Images/MainNavBar/Image6.webp',
+  'Images/MainNavBar/Image7.webp',
+  'Images/MainNavBar/Image8.webp',
+  'Images/MainNavBar/Image9.jpg',
+  'Images/MainNavBar/Image10.jpg',
+
+];
 
 
 
-  
-  var index=0;
-  var koha=2500;
-  
-  
-  function krijoSlider(){
-  document.getElementById('imazhi').src=vargu[index];
+
+var index = 0;
+var koha = 2500;
+
+
+function krijoSlider() {
+  document.getElementById('imazhi').src = vargu[index];
   index++;
-  if(index==vargu.length){
-  index=0;
-  }
-  
-  setTimeout("krijoSlider()",koha);
+  if (index == vargu.length) {
+    index = 0;
   }
 
-  krijoSlider();
-
-  //teksti me slider
-  const slider = document.querySelector('.slider');
-  const slides = document.querySelectorAll('.slide');
-  
-  let currentSlide = 0;
-  let slideInterval = setInterval(nextSlide, 2500);
-  
-  function nextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-  }
-  
-//lojerave slider
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const slides2 = document.querySelectorAll('.slide2');
-
-let currentSlide2 = 0;
-
-// Hide all but the first four slides
-for (let i = 4; i < slides2.length; i++) {
-  slides2[i].style.display = 'none';
+  setTimeout("krijoSlider()", koha);
 }
 
-prevButton.addEventListener('click', () => {
-  // Hide the current set of four slides
-  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
-    slides2[i].style.display = 'none';
-  }
-  // Decrement the current slide index and update the display of the previous set of four slides
-  currentSlide2 = (currentSlide2 - 4 + slides2.length) % slides2.length;
-  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
-    slides2[i].style.display = 'block';
-  }
-});
+krijoSlider();
 
-nextButton.addEventListener('click', () => {
-  // Hide the current set of four slides
-  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
-    slides2[i].style.display = 'none';
+//teksti me slider
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 2500);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+//slider i dyte
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides2.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
   }
-  // Increment the current slide index
-  currentSlide2 = (currentSlide2 + 4) % slides2.length;
-  // If there are fewer than four slides remaining, show the remaining slides and then show the first four slides again
-  if (currentSlide2 + 4 > slides2.length) {
-    let remainingSlides = currentSlide2 + 4 - slides2.length;
-    for (let i = currentSlide2; i < slides2.length; i++) {
-      slides2[i].style.display = 'block';
-    }
-    for (let i = 0; i < 4; i++) {
-      slides2[i].style.display = 'block';
-    }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  // Update the display of the next set of four slides
-  for (let i = currentSlide2; i < currentSlide2 + 4; i++) {
-    slides2[i].style.display = 'block';
-  }
-});
+  slides2[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
